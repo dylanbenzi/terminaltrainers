5e63e0e83cfd6a98fbf128d6896a6f66b252b8e6
dylanbenzi <dylanbenzi@gmail.com>
2025-03-05 15:57:42 -0500

    add map rendering

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <BearLibTerminal.h>
 #include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include "json.hpp"
 
@@ -8,23 +9,28 @@ using json = nlohmann::json;
 using namespace std;
 
 struct Tile {
-	int tileSize;
 	int x;
 	int y;
 	int tileOffset;
-	int tileLayer;
-	string layerName;
 	bool collisionTile;
 };
 
 class TileMap {
 private:
 	vector<Tile> tiles;
-	map<pair<int, int>, Tile> tilesByPosition;
+	int layers;
 	int width, height;
-
+	int baseTileCode = 0xE000;
+	int cellPixelsX = 8;
+	int cellPixelsY = 4;
+	//map<tuple<int, int, int>, Tile> tilesByPosition;
+	vector<map<pair<int, int>, Tile>> tileLayerMap;
 public:
-	bool loadMapJson(string& filename) {
+	TileMap(): width(0), height(0), layers(0) {
+		tileLayerMap.resize(10);
+	}
+
+	bool loadMapJson(const string& filename) {
 		try {
 			ifstream file(filename);
 			
@@ -33,46 +39,76 @@ public:
 			json mapJson;
 			file >> mapJson;
 
-			tiles.clear();
-			tilesByPosition.clear();
-			
 			width = mapJson["mapWidth"];
 			height = mapJson["mapHeight"];
-			
-			int layer = 0;
 			int globalTileSize = mapJson["tileSize"];
 
-			for(auto& tileLayers : mapJson["layers"]) {
-				string tileLayersName = tileLayers["name"];
-				bool tileLayersCollider = tileLayers["collider"];
+			auto mapLayer = mapJson["layers"];
+			layers = mapLayer.size();
+			
+			tileLayerMap.resize(layers);
+			cout << mapJson["layers"].size() << endl;
+
+			int drawLayer = 0;
+
+			for(int layer = layers - 1; layer >= 0; layer--) {
+				string tileLayersName = mapLayer[layer]["name"];
+				cout << tileLayersName << endl;
+				bool tileLayersCollider = mapLayer[layer]["collider"];
+				cout << tileLayersCollider << endl;
 				
-				for(auto& tilesArr : tileLayers["tiles"]) {
+				auto mapLayerArr = mapLayer[layer]["tiles"];
+
+				for(int tileList = 0; tileList < mapLayerArr.size(); tileList++) {
+					auto mapLayerTilesArr = mapLayerArr[tileList];
+
 					Tile tile;
 
-					tile.tileOffset = tilesArr["id"];
-					tile.x = tilesArr["x"];
-					tile.y = tilesArr["y"];
-					tile.layerName = tileLayersName;
+					cout << "[" << layer << ", " << tileList << "]" << endl;
+
+					string tileIdStr = mapLayerTilesArr["id"];
+
+					cout << tileIdStr << endl;
+					
+					int tileIdInt = stoi(tileIdStr);
+
+					//cout << "Tile: (" << tile.x << ", " << tile.y << "): " << tile.tileOffset << "; Layer: " << tile.tileLayer << endl;
+
+					tile.tileOffset = tileIdInt;
+					tile.x = mapJson["layers"][layer]["tiles"][tileList]["x"];
+					cout << "x: " << tile.x << endl;
+					tile.y = mapJson["layers"][layer]["tiles"][tileList]["y"];
+					cout << "y: " << tile.y << endl;
 					tile.collisionTile = tileLayersCollider;
-					tile.tileSize = globalTileSize;
 
 					tiles.push_back(tile);
-					tilesByPosition[{tile.x, tile.y}] = tile;
+
+					cout << "tiles push ok" << endl;
+
+					tileLayerMap[drawLayer][{tile.x, tile.y}] = tile;
+
+					cout << "layer map push ok" << endl;
 				}
+
+				drawLayer++;
 				
 			}
-
+			
 			return true;
 		}catch(exception& e){
+			cout << e.what() << endl;
+
 			return false;			
 		}
 	}
 
-	bool getTile(int x, int y, Tile& returnTile) const {
+	bool getTile(int x, int y, int layer, Tile& returnTile) const {
 		Tile tile;
-		auto findTile = tilesByPosition.find({x, y});
+		auto findTile = tileLayerMap[layer].find({x, y});
 
-		if(findTile != tilesByPosition.end()) {
+		//cout << findTile->second.tileOffset;
+
+		if(findTile != tileLayerMap[layer].end()) {
 			returnTile = findTile -> second;
 			return true;
 		}else{
@@ -80,18 +116,34 @@ public:
 		}
 	}
 
-	bool getCollision(int x, int y) {
+	bool getCollision(int x, int y, int layer) const {
 		Tile tile;
 		
-		if(getTile(x, y, tile)) {
+		if(getTile(x, y, layer, tile)) {
 			return tile.collisionTile;
 		}else{
 			return false;
 		}
 	}
 
-	void render() {
+	void render(int x, int y, int viewWidth, int viewHeight) {
+		for(int tileLayer = 0; tileLayer < layers; tileLayer++) {
+			for(int tilesX = 0; tilesX < viewWidth; tilesX++) {
+				for(int tilesY = 0; tilesY < viewHeight; tilesY++) {
+					Tile tile;
 
+					if(getTile(x + tilesX, y + tilesY, tileLayer, tile)) {
+						int xCoord = (tilesX * cellPixelsX) + x;
+						int yCoord = (tilesY * cellPixelsY) + y;
+						int tileCode = baseTileCode + tile.tileOffset;
+
+						cout << "Tile: (" << tilesX << ", " << tilesY << "): " << tile.tileOffset << "; Layer: " << tileLayer << endl;
+						terminal_layer(tileLayer);
+						terminal_put_ext(xCoord, yCoord, 0, 0, tileCode); 
+					}
+				}
+			}
+		}
 	}
 
 	int getWidth() {
@@ -105,23 +157,37 @@ public:
 };
 
 int main() {
-	TileMap map;
+	TileMap gameMap;
+
+	Tile tile;
+	if(!gameMap.loadMapJson("./map/map.json")) {
+		cout << "Map error";
+		return 1;
+	};
+
+	//if(!gameMap.getTile(0, 0, 0, tile)) {
+	//	cout << "Tile error";
+	//	return 1;
+	//};
 
 	int windowX = 136;
 	int windowY = 68;
 
-	int cellPixelsX = 8;
-	int cellPixelsY = 4;
+	int renderX = 0;
+	int renderY = 0;
 
-	int windowXCenter = (windowX / 2) - (cellPixelsX / 2);
-	int windowYCenter = (windowY / 2) - (cellPixelsY / 2);
+	//int cellPixelsX = 8;
+	//int cellPixelsY = 4;
 
-	int baseBackground = 0xE000;
-	int grassTile = baseBackground + 45;
-	int oneRedFlower = baseBackground + 7;
+	//int windowXCenter = (windowX / 2) - (cellPixelsX / 2);
+	//int windowYCenter = (windowY / 2) - (cellPixelsY / 2);
 
-	int baseCharacter = 0xF000;
-	int guyCharacter = baseCharacter + 4;
+	//int baseBackground = 0xE000;
+	//int grassTile = baseBackground + 45;
+	//int oneRedFlower = baseBackground + 7;
+
+	//int baseCharacter = 0xF000;
+	//int guyCharacter = baseCharacter + 4;
 
 	terminal_open();
 
@@ -130,27 +196,29 @@ int main() {
 	bool running = true;
 	
 	while (running) {
+		terminal_set("0xE000: ./map/spritesheet.png, size=64x64, align=top-left");
 
-		terminal_set("0xE000: ./tiles/background.png, size=64x64, align=top-left");
-		terminal_set("0xF000: ./tiles/characters.png, size=64x64, align=top-left");
+		//terminal_set("0xE000: ./tiles/background.png, size=64x64, align=top-left");
+		//terminal_set("0xF000: ./tiles/characters.png, size=64x64, align=top-left");
 
-		for(int i = 0; i <= windowY; i+=cellPixelsY){
-			for(int j = 0; j <= windowX; j+=cellPixelsX){
-				int tileCode = grassTile;
+		//for(int i = 0; i <= windowY; i+=cellPixelsY){
+		//	for(int j = 0; j <= windowX; j+=cellPixelsX){
+		//		int tileCode = grassTile;
+		//
+		//		terminal_put_ext(j, i, 0, 0, tileCode);
+		//	}
+		//}
 
-				terminal_put_ext(j, i, 0, 0, tileCode);
-			}
-		}
+		//terminal_layer(1);
+		//terminal_put_ext(windowXCenter, windowYCenter, 0, 0, guyCharacter);
 
-		terminal_layer(1);
-		terminal_put_ext(windowXCenter, windowYCenter, 0, 0, guyCharacter);
+		gameMap.render(renderX, renderY, 15, 15);
 
 		int key = terminal_read();
 		if(key == TK_ESCAPE) {
 			running = false;
 		}
 
-		if(key == TK_W) {}
 
 
 		terminal_refresh();
